7ed30aacadc0eafbb2e9565a84d9efe8c7bbf3e0
Dani Manuel Céspedes Lara <dmanuelcl@gmail.com>
2018-08-31 19:53:29 -0400

    `apiParameters` on AppServices.ApiParameter now return valid result

--- AppServices/AppServices/Networking/ApiParameter.swift
+++ AppServices/AppServices/Networking/ApiParameter.swift
@@ -17,7 +17,22 @@ enum ApiParameter: ApiParameterConvertible {
     case minmagnitude(Double)
     
     func apiParameters() -> [AnyHashable : Any] {
-        return [:]
+        switch self {
+        case .starttime(let date):
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZ"
+            return ["starttime": dateFormatter.string(from: date)]
+        case .latitude(let latitude):
+            return ["latitude": latitude]
+        case .longitude(let longitude):
+            return ["longitude": longitude]
+        case .maxradiuskm(let km):
+            return ["maxradiuskm": km]
+        case .limit(let limit):
+            return ["limit": limit]
+        case .minmagnitude(let magnitude):
+            return ["minmagnitude": magnitude]
+        }
     }
 }
 
