9180dd5e104d6c6ac9b930187400977cc5234869
Dani Manuel Céspedes Lara <dmanuelcl@gmail.com>
2018-08-31 15:49:32 -0400

    Initial Commit

---
+++ Earthquakes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Earthquakes
+//
+//  Created by Dani Manuel Céspedes Lara on 8/31/18.
+//  Copyright © 2018 Dani Manuel Céspedes Lara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
